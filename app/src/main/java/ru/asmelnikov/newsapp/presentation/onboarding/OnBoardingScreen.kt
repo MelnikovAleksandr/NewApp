0c41e0d761a5d1fb5458ee4d7f08f50b3ff6c425
Alexander Melnikov <melnikov150990@mail.ru>
2023-08-14 02:10:25 +0500

    on boarding screen

---
+++ app/src/main/java/ru/asmelnikov/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,90 @@
+package ru.asmelnikov.newsapp.presentation.onboarding
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
+import ru.asmelnikov.newsapp.presentation.Dimens.MediumPadding2
+import ru.asmelnikov.newsapp.presentation.Dimens.PageIndicatorWidth
+import ru.asmelnikov.newsapp.presentation.common.NewButton
+import ru.asmelnikov.newsapp.presentation.common.NewTextButton
+import ru.asmelnikov.newsapp.presentation.common.PageIndicator
+import ru.asmelnikov.newsapp.presentation.onboarding.components.OnBoardingPage
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun OnBoardingScreen() {
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        val pagerState = rememberPagerState(initialPage = 0) {
+            pages.size
+        }
+
+        val buttonState = remember {
+            derivedStateOf {
+                when (pagerState.currentPage) {
+                    0 -> listOf("", "Next")
+                    1 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
+                    else -> listOf("", "")
+                }
+            }
+        }
+
+        HorizontalPager(state = pagerState) { index ->
+            OnBoardingPage(page = pages[index])
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MediumPadding2)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(
+                modifier = Modifier.width(PageIndicatorWidth),
+                pageSize = pages.size,
+                selectedPage = pagerState.currentPage
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                val scope = rememberCoroutineScope()
+
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewTextButton(text = buttonState.value[0], onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(page = pagerState.currentPage - 1)
+                        }
+                    })
+                }
+
+                NewButton(text = buttonState.value[1]) {
+                    scope.launch {
+                        if (pagerState.currentPage == 3) {
+                            // nav
+                        } else {
+                            pagerState.animateScrollToPage(
+                                page = pagerState.currentPage + 1
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.weight(0.5f))
+    }
+}
